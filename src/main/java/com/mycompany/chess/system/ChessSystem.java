138698023a73e1b40a64623450056ed018722213
Dnysss <denysantos2014@hotmail.com>
2024-04-22 16:47:22 -0300

    project created

---
+++ src/main/java/com/mycompany/chess/system/ChessSystem.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.chess.system;
+
+/**
+ *
+ * @author dnys
+ */
+public class ChessSystem {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
