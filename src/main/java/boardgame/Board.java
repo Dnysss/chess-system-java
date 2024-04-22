1b5f5d688843b8c5c7023a41df60c1d508dc1c89
Dnysss <denysantos2014@hotmail.com>
2024-04-22 18:41:59 -0300

    starting to implement Board and Piece

---
+++ src/main/java/boardgame/Board.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package boardgame;
+
+/**
+ *
+ * @author dnys
+ */
+public class Board {
+    private int rows;
+    private int columns;
+    private Piece[][] pieces;
+
+    public Board(int rows, int coluns) {
+        this.rows = rows;
+        this.columns = columns;
+        pieces = new Piece[rows][columns];
+    }
+
+    public int getRows() {
+        return rows;
+    }
+
+    public void setRows(int rows) {
+        this.rows = rows;
+    }
+
+    public int getColumns() {
+        return columns;
+    }
+
+    public void setColumns(int columns) {
+        this.columns = columns;
+    }
+    
+}
